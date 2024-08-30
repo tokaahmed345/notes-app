73e322fd0a54e1b0063443f5e92253661fa4800e
tokaahmed345 <tokaahmed34567@gmail.comgit config user.name 'tokaahmed345'git config user.email>
2024-08-31 02:25:37 +0300

    change theme and create notesview

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,22 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
+import 'package:noteapp/views/notesview.dart';
 
-
-
-
-void main(){
-runApp(NotesApp());
+void main() {
+  runApp(const NotesApp());
 }
-
-
 
 class NotesApp extends StatelessWidget {
   const NotesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark),
+      routes: {
+        NotesView.id: (context) => NotesView(),
+      },
+      initialRoute: NotesView.id,
+    );
   }
 }
