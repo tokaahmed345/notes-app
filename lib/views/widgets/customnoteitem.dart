b73309d75b03ac3fe97192336d610d23d20acd7c
tokaahmed345 <tokaahmed34567@gmail.comgit config user.name 'tokaahmed345'git config user.email>
2024-08-31 23:06:59 +0300

    create note model

--- lib/views/widgets/customnoteitem.dart
+++ lib/views/widgets/customnoteitem.dart
@@ -1,26 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:noteapp/constant.dart';
 import 'package:noteapp/views/editnoteview.dart';
 
 class NoteItem extends StatelessWidget {
-  const NoteItem({super.key,required this.color});
-   final  Color color;
+  const NoteItem({super.key}); //required this.color});
+  //final  Color color;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: (){
+      onTap: () {
         Navigator.push(context, MaterialPageRoute(builder: (context) {
-return const EditNoteView();
+          return const EditNoteView();
         }));
-       
-       
       },
       child: Container(
-        
         padding: EdgeInsets.only(top: 24, bottom: 24, left: 16),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(16),
-          color: color
+          color: //color
+              Color(0xffFFCC80),
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.end,
@@ -50,8 +49,8 @@ return const EditNoteView();
               padding: const EdgeInsets.only(right: 24.0),
               child: Text(
                 "May 21,2022",
-                style:
-                    TextStyle(color: Colors.black.withOpacity(.4), fontSize: 16),
+                style: TextStyle(
+                    color: Colors.black.withOpacity(.4), fontSize: 16),
               ),
             )
           ],
--- lib/views/widgets/noteslistview.dart
+++ lib/views/widgets/noteslistview.dart
@@ -3,29 +3,29 @@ import 'package:noteapp/views/widgets/customnoteitem.dart';
 
 class NotesListView extends StatelessWidget {
   const NotesListView({super.key});
-  final List data = const [
-   Color(0xffFFCC80),
-   Color.fromARGB(255, 215, 248, 150),
-   Color.fromARGB(255, 52, 117, 228),
-   Color.fromARGB(255, 216, 92, 133),
-   Color.fromARGB(255, 169, 77, 186),
-   Color(0xffFFCC80),
-   Color.fromARGB(255, 215, 248, 150),
-   Color.fromARGB(255, 52, 117, 228),
-   Color.fromARGB(255, 216, 92, 133),
-   Color.fromARGB(255, 169, 77, 186),
-  ];
+  // final List data = const [
+  //  Color(0xffFFCC80),
+  //  Color.fromARGB(255, 215, 248, 150),
+  //  Color.fromARGB(255, 52, 117, 228),
+  //  Color.fromARGB(255, 216, 92, 133),
+  //  Color.fromARGB(255, 169, 77, 186),
+  //  Color(0xffFFCC80),
+  //  Color.fromARGB(255, 215, 248, 150),
+  //  Color.fromARGB(255, 52, 117, 228),
+  //  Color.fromARGB(255, 216, 92, 133),
+  //  Color.fromARGB(255, 169, 77, 186),
+  // ];
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 16.0),
       child: ListView.builder(
         padding: EdgeInsets.zero,
-        itemCount: data.length,
+     //   itemCount: data.length,
         itemBuilder: (context, index) {
         return Padding(
           padding: const EdgeInsets.symmetric(vertical: 4.0),
-          child: NoteItem(color: data[index],),
+          child: NoteItem()//)color: data[index],),
         );
       }),
     );
