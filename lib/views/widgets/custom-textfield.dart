ead8cfada8ae825ee7eeddaaf2e2d544674940f4
tokaahmed345 <tokaahmed34567@gmail.comgit config user.name 'tokaahmed345'git config user.email>
2024-09-01 22:53:32 +0300

    validate user input

--- lib/views/widgets/custom-textfield.dart
+++ lib/views/widgets/custom-textfield.dart
@@ -2,13 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:noteapp/constant.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key,required this.hint,this.maxline=1});
+  const CustomTextField(
+      {super.key, required this.hint, this.maxline = 1, this.onSaved});
   final String hint;
   final int maxline;
+  final void Function(String?)? onSaved;
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      maxLines: maxline,
+    return TextFormField(
+        onSaved: onSaved,
+        validator: (value) {
+          //c hech if value exits or not
+          if (value?.isEmpty ?? true) {
+            return 'field is required ';
+          }
+        },
+        maxLines: maxline,
         cursorColor: PKColor,
         decoration: InputDecoration(
           hintText: hint,
--- lib/views/widgets/custombutton.dart
+++ lib/views/widgets/custombutton.dart
@@ -1,23 +1,25 @@
-
 import 'package:flutter/material.dart';
 import 'package:noteapp/constant.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key});
-
+  const CustomButton({super.key,this.ontap});
+   final void Function()? ontap;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 55,
-      width: MediaQuery.of(context).size.width,
-      decoration:
-          BoxDecoration(color: PKColor, borderRadius: BorderRadius.circular(8)),
-      child: const Center(
-          child: Text(
-        "Add",
-        style: TextStyle(
-            fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
-      )),
+    return GestureDetector(
+      onTap: ontap,
+      child: Container(
+        height: 55,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+            color: PKColor, borderRadius: BorderRadius.circular(8)),
+        child: const Center(
+            child: Text(
+          "Add",
+          style: TextStyle(
+              fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
+        )),
+      ),
     );
   }
 }
